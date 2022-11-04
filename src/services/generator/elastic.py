5e598ec4fcaed995f2b2c09b7b9331cf4a16986e
Vlad Efremov <vladefr97@yandex.ru>
2022-11-04 16:07:58 +0300

    feat: попытки улучшить поиск

--- src/services/generator/elastic.py
+++ src/services/generator/elastic.py
@@ -5,8 +5,7 @@ from functools import lru_cache
 
 class EDSLGenerator:
     def generate_knowledge_publication_text_edsl(self, publication_text: str) -> Dict[str, Any]:
-        # TODO: попробовать multi_match
-        return {"query": {"multi_match": {"query": publication_text, "fields": ["text"], "type": "most_fields"}}}
+        return {"query": {"match": {"text": {"query": publication_text, "fuzziness": "auto"}}}}
 
 
 @lru_cache()
--- src/storage/instances/elastic/settings.py
+++ src/storage/instances/elastic/settings.py
@@ -9,6 +9,10 @@ KNOWLEDGE_INDEX_SETTINGS = {
                 "english_possessive_stemmer": {"type": "stemmer", "language": "possessive_english"},
                 "russian_stop": {"type": "stop", "stopwords": "_russian_"},
                 "russian_stemmer": {"type": "stemmer", "language": "russian"},
+                "custom_stopwords": {
+                    "type": "stop",
+                    "stopwords": "а,без,более,бы,был,была,были,было,быть,в,вам,вас,весь,во,вот,все,всего,всех,вы,где,да,даже,для,до,его,ее,если,есть,еще,же,за,здесь,и,из,или,им,их,к,как,ко,когда,кто,ли,либо,мне,может,мы,на,надо,наш,не,него,нее,нет,ни,них,но,ну,о,об,однако,он,она,они,оно,от,очень,по,под,при,с,со,так,также,такой,там,те,тем,то,того,тоже,той,только,том,ты,у,уже,хотя,чего,чей,чем,что,чтобы,чье,чья,эта,эти,это,я,a,an,and,are,as,at,be,but,by,for,if,in,into,is,it,no,not,of,on,or,such,that,the,their,then,there,these,they,this,to,was,will,with",  # noqa: E501
+                },
             },
             "analyzer": {
                 "ru_en": {
@@ -20,6 +24,7 @@ KNOWLEDGE_INDEX_SETTINGS = {
                         "english_possessive_stemmer",
                         "russian_stop",
                         "russian_stemmer",
+                        "custom_stopwords",
                     ],
                 }
             },
@@ -33,11 +38,11 @@ KNOWLEDGE_INDEX_SETTINGS = {
             # ID  автора сообщения
             "author_id": {"type": "long"},
             # Имя автора сообщения
-            "author_first_name": {"type": "text", "analyzer": "ru_en"},
+            "author_first_name": {"type": "keyword"},
             # Фамилия автора сообщения
-            "author_last_name": {"type": "text", "analyzer": "ru_en"},
+            "author_last_name": {"type": "keyword"},
             # Логин автора сообщения
-            "author_login": {"type": "text", "analyzer": "ru_en"},
+            "author_login": {"type": "keyword"},
             # ID канала куда опубликовали сообщение
             "published_channel_id": {"type": "long"},
             # ID опубликованного в канале сообщения
